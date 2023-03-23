f67e43d93e28ec7632cd7481e2927aa407331617
joshwoo4018 <joshwoo4018@gmail.com>
2023-03-22 22:32:42 -0700

    commmt

--- q1.hpp
+++ q1.hpp
@@ -9,3 +9,25 @@ using namespace std;
 // ******************************
 // Implement all your functions here
 // ******************************
+void getinput(int &, int &);
+void getinput(float &, float &);
+void swapTwoValues(int &, int &);
+void swapTwoValues(float &, float &);
+
+void getinput(int &n1, int &n2) {
+    cout << "Enter 2 values: " << endl;
+    cin >> n1 >> n2;
+} void getinput (float &f1, float &f2) {
+    cout << "Enter 2 values: " << endl;
+    cin >> f1 >> f2;
+} void swapTwoValues(int &n1, int &n2) {
+    int tap;
+    tap = n1;
+    n1 = n2;
+    n2 = tap;
+} void swapTwoValues(float &f1, float &f2) {
+    float tap;
+    tap = f1;
+    f1 = f2;
+    f2 = tap;
+}
